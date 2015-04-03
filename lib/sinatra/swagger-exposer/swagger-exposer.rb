9dace3db5d989a5bd29d4ee3d47163daf553894d
Julien Kirch <code@archiloque.net>
2015-04-03 15:11:29 +0200

    Start to work

--- lib/sinatra/swagger-exposer/swagger-exposer.rb
+++ lib/sinatra/swagger-exposer/swagger-exposer.rb
@@ -3,61 +3,228 @@ require 'json'
 
 module Sinatra
 
+  module SwaggerContentCreator
+
+    def self.create_swagger_content(types, endpoints, info)
+      result_endpoints = {}
+
+      # swagger need the endpoints to be grouped by path
+      endpoints_by_path = endpoints.group_by { |endpoint| endpoint[:path] }
+      endpoints_by_path.keys.sort.each do |path|
+        endpoints = endpoints_by_path[path]
+
+        result_endpoints_for_path = {}
+        endpoints.each do |endpoint|
+          result_endpoint = create_swagger_endpoint(endpoint)
+          result_endpoints_for_path[endpoint[:type]] = result_endpoint
+        end
+
+        result_endpoints[path] = result_endpoints_for_path
+      end
+      result_types = {}
+      types.each_pair do |name, value|
+        result_types[name] = create_swagger_type(value)
+      end
+
+      result = {
+          swagger: '2.0',
+          consumes: [
+              'application/json'
+          ],
+          produces: [
+              'application/json'
+          ],
+          paths: result_endpoints,
+          definitions: result_types,
+      }
+      if info
+        result[:info] = info
+      end
+
+      result
+    end
+
+    def self.create_swagger_type(type)
+      result_type = {}
+
+      if type.key? :properties
+        result_properties = {}
+        type[:properties].each_pair do |property_name, property_info|
+          result_property = create_swagger_type_property(property_name, property_info)
+          if result_property
+            result_properties[property_name] = result_property
+          end
+        end
+        unless result_properties.empty?
+          result_type[:properties] = result_properties
+        end
+      end
+
+      [:required, :example].each do |property_key|
+        if type.key? property_key
+          result_type[property_key] = type[property_key]
+        end
+      end
+
+      result_type
+    end
+
+    KNOWN_SWAGGER_TYPE_PROPERTIES = [:type, :format, :description]
+
+    def self.create_swagger_type_property(property_name, property_info)
+      result = {}
+      KNOWN_SWAGGER_TYPE_PROPERTIES.each do |known_property|
+        if property_info.key? known_property
+          property_value = property_info[known_property]
+          if property_value.is_a? Class
+            property_value = property_value.to_s.downcase
+          end
+          result[known_property] = property_value
+        end
+      end
+      property_info.each_pair do |property_name, property_value|
+        unless KNOWN_SWAGGER_TYPE_PROPERTIES.include? property_name
+          if logging?
+            $stderr.puts "Swagger: unknown property [#{property_name}] for #{property_name}: #{property_info}"
+          end
+        end
+      end
+
+      result.empty? ? nil : result
+    end
+
+    def self.create_swagger_endpoint(endpoint)
+      result_endpoint = {
+          produces: ['application/json']
+      }
+      [:summary, :description].each do |property_name|
+        if endpoint[property_name]
+          result_endpoint[property_name] = endpoint[property_name]
+        end
+      end
+      responses = endpoint[:responses]
+      if responses && (!responses.empty?)
+        result_responses = {}
+        responses.each_pair do |code, content|
+          result_response = {
+              :description => content[:description],
+          }
+          if content[:params] && content[:params][:type]
+            result_schema = {
+                :type => content[:params][:type],
+                :items => {
+                    '$ref' => "#/definitions/#{content[:type]}",
+                }
+            }
+            result_response[:schema] = result_schema
+          else
+            result_schema = {
+                '$ref' => "#/definitions/#{content[:type]}",
+            }
+            result_response[:schema] = result_schema
+          end
+          result_responses[code] = result_response
+        end
+        result_endpoint[:responses] = result_responses
+      end
+      result_endpoint
+    end
+
+  end
+
   # Expose swagger API from your Sinatra app
   module SwaggerExposer
 
     def self.registered(app)
       app.set :swagger_endpoints, []
       app.set :swagger_current_endpoint, {}
+      app.set :swagger_types, {}
 
+      # Declare the swagger endpoint
       app.get '/swagger_doc.json' do
-        endpoints_by_path = settings.swagger_endpoints.group_by { |e| e[:path] }
-        result_endpoints = {}
-
-        endpoints_by_path.each_pair do |path, endpoints|
-          result_endpoints_for_path = {}
-          endpoints.each do |endpoint|
-            result_endpoint = {}
-            if endpoint[:description]
-              result_endpoint[:description] = endpoint[:description]
-            end
-            result_endpoints_for_path[endpoint[:type]] = result_endpoint
-          end
-          result_endpoints[path] = result_endpoints_for_path
-        end
+        result_endpoints = SwaggerContentCreator.create_swagger_content(
+            settings.swagger_types,
+            settings.swagger_endpoints,
+            settings.swagger_info
+        )
+        STDOUT << "#{result_endpoints}\n"
         content_type :json
-        {
-            swagger: '2.0',
-            consumes: [
-                'application/json'
-            ],
-            produces: [
-                'application/json'
-            ],
-            paths: result_endpoints,
-            definitions: {},
-        }.to_json
+        result_endpoints.to_json
       end
     end
 
-    private
-
-    def process_endpoint(type, args)
-      current_endpoint = settings.swagger_current_endpoint
-      current_endpoint[:type] = type
-      current_endpoint[:path] = args[0]
-      if logging?
-        $stderr.puts "Swagger: found new endpoint #{current_endpoint}"
-      end
-      settings.swagger_endpoints << current_endpoint
-      set :swagger_current_endpoint, {}
+    # Provide a summary for the endpoint
+    def summary(text)
+      settings.swagger_current_endpoint[:summary] = text
     end
 
-    public
-
+    # Provide a description for the endpoint
     def description(text)
       settings.swagger_current_endpoint[:description] = text
     end
+
+    # Known fields for the info field
+    INFO_FIELDS = {
+        :version => String,
+        :title => String,
+        :description => String,
+        :termsOfService => String,
+        :contact => {:name => String, :email => String, :url => String, },
+        :license => {:name => String, :url => String, },
+    }
+
+    # General information
+    def swagger_info(params)
+      set :swagger_info, validate_params(params, INFO_FIELDS, 'info', params)
+    end
+
+    # Declare a type
+    def type(name, params)
+      types = settings.swagger_types
+      if types.key? name
+        raise "Type [#{name}] already exist with value #{types[name]}"
+      end
+      types[name] = params
+    end
+
+    # Declare a response
+    def response(code, description, type, params = nil)
+      current_endpoint = settings.swagger_current_endpoint
+      unless current_endpoint.key? :responses
+        current_endpoint[:responses] = {}
+      end
+      if current_endpoint[:responses].key? code
+        raise "Response [#{code}] already exist with value #{current_endpoint[:responses][code]}"
+      end
+      if type.is_a?(String) && (!settings.swagger_types.key?(type))
+        raise "Unknown type [#{type}], registered types are #{settings.swagger_types.keys.join(', ')}"
+      end
+
+      current_response = {
+          :description => description,
+          :type => type,
+      }
+
+      if params
+        params.each_pair do |param_key, param_value|
+          unless [:type].include? param_key
+            raise "Unknown parameter [#{param_key}]"
+          end
+          if params[:type]
+            type_value = params[:type]
+            if ['array', Array].include? type_value
+              params[:type] = 'array'
+            else
+              raise "Unknown type [#{type_value}]"
+            end
+          end
+        end
+        current_response[:params] = params
+      end
+
+      current_endpoint[:responses][code] = current_response
+    end
+
 
     def delete(*args, &block)
       process_endpoint('delete', args)
@@ -102,6 +269,51 @@ module Sinatra
     def unlink(*args, &block)
       process_endpoint('unlink', args)
       super(*args, &block)
+    end
+
+    private
+
+    # Call for each endpoint declaration
+    def process_endpoint(type, args)
+      current_endpoint = settings.swagger_current_endpoint
+      current_endpoint[:type] = type
+      current_endpoint[:path] = args[0]
+      if logging?
+        $stderr.puts "Swagger: found new endpoint #{current_endpoint}"
+      end
+      settings.swagger_endpoints << current_endpoint.clone
+      settings.swagger_current_endpoint.clear
+    end
+
+    def validate_params(values, known_fields, field_name, general_values)
+      result = {}
+      values.each_pair do |key, value|
+        key_sym = key.to_sym
+        if known_fields.key? key_sym
+          known_value = known_fields[key_sym]
+          if known_value == String
+            if value.is_a? String
+              result[key_sym] = value
+            else
+              $stderr.puts "Swagger: property [#{key}] value [#{value}] should be a String for #{field_name}: #{general_values}"
+            end
+          else
+            if value.is_a? Hash
+              sub_params = validate_params(value, known_value, field_name, general_values)
+              if sub_params
+                result[key_sym] = sub_params
+              end
+            else
+              $stderr.puts "Swagger: property [#{key}] value [#{value}] should be a Hash for #{field_name}: #{general_values}"
+            end
+          end
+        else
+          if logging?
+            $stderr.puts "Swagger: unknown property [#{key}] for #{field_name}: #{general_values}"
+          end
+        end
+      end
+      result.empty? ? nil : result
     end
 
   end
